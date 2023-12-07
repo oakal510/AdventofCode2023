24b8f3193fe1ad31c2628c8208e9b47151231d81
Alla Barbalat <allabarbalat@gmail.com>
2023-12-06 16:36:46 -0800

    advent of code, day 6, solution 2

---
+++ day_06/solution_part_2.py
@@ -0,0 +1,47 @@
+# advent of code day 6, part 1
+
+def calculate_product_of_wins(buffer):
+
+    values = buffer.splitlines()
+    time = ''
+    distance = ''
+    
+    for value in values[0].strip():
+        if value.isnumeric():
+            time += value
+        print(time)
+
+    for value in values[1].strip():
+        if value.isnumeric():
+            distance += value
+        print(distance)
+
+    return win_counter(time, distance)
+
+
+def win_counter(time, distance):
+
+    win_count = 0
+
+    for charge in range(int(time)):
+        time_remaining = int(time) - charge
+            
+        if charge * time_remaining > int(distance):
+            win_count += 1
+
+    return win_count
+
+
+def test_solution():
+
+    with open('test_input.txt', 'r') as file:
+        buffer = file.read()
+    
+    assert calculate_product_of_wins(buffer) == 71503
+
+
+if __name__ == "__main__":
+
+    with open('input.txt', 'r') as file:
+        buffer = file.read()
+        print(calculate_product_of_wins(buffer))
